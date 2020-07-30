069ac03308ecc7d8505389abd94f86d56a1bbce1
Taylor Cooke <taylorcooke@gmail.com>
2020-07-30 14:32:15 -0400

    added components and router

---
+++ blog-app/src/router.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Index from '@/components/Index'
+import Create from '@/components/Create'
+import Edit from '@/components/Edit'
+
+Vue.use(VueRouter)
+
+export default new VueRouter({
+    routes: [
+        {
+            path: '/',
+            redirect: '/index'
+        },
+        {
+            path: '/create',
+            name: 'create',
+            component: Create
+        },
+        {
+            path: '/edit/:id',
+            name: 'edit',
+            component: Edit
+        },
+        {
+            path: '/index',
+            name: 'index',
+            component: Index
+        }
+    ]
+})
