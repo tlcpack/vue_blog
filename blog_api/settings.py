b63a944c3ccbe03bb8f0a8edf963fb54d58fbb9a
Taylor Cooke <taylorcooke@gmail.com>
2020-08-13 22:03:39 -0400

    added some static files stuff for heroku

--- blog_api/settings.py
+++ blog_api/settings.py
@@ -120,7 +120,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
