873579a8d4d536058d73042bf8f38b23154dc1f2
Taylor Cooke <taylorcooke@gmail.com>
2020-08-19 20:40:30 -0400

    fixing so that centering only affects blog title

--- blog-app/src/App.vue
+++ blog-app/src/App.vue
@@ -2,14 +2,15 @@
   <div class="container">
     <div class="row justify-content-center">
       <div class="col-md-8">
-        <h2 class="d-flex justify-content-center">
+        <h2 class="row justify-content-center">
           <router-link to="/index">Blog Time!</router-link>
         </h2>
         <div class="row justify-content-center">
         <router-link to='/create' class="m-3 btn btn-sm btn-primary">Create post</router-link>
         <router-link to='/seasons' class="m-3 btn btn-sm btn-secondary">List of seasons</router-link>
-        <router-view />
         </div>
+        <router-view />
+        
       </div>  
     </div>
   </div>
