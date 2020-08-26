c35416facc902b81f95b8a130853e12594821f71
Taylor Cooke <taylorcooke@gmail.com>
2020-08-25 20:34:51 -0400

    comment model added

--- blog/models.py
+++ blog/models.py
@@ -33,3 +33,15 @@ class Season(models.Model):
 
     def __str__(self):
         return str(self.year)
+
+class Comment(models.Model):
+    content = models.TextField(max_length=500)
+    comment_date_added = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name="comments")
+
+    class Meta:
+        ordering = ['comment_date_added']
+
+    def __str__(self):
+        return self.content
+    
