f31148fe1452eac8b2e39e9aecbf26b1d0a78d22
Taylor Cooke <taylorcooke@gmail.com>
2020-08-27 21:28:24 -0400

    update seasons detail page

--- blog-app/src/components/Season.vue
+++ blog-app/src/components/Season.vue
@@ -2,6 +2,10 @@
   <div class="p-3">
     <h3 class="m-2">{{ season.year }}</h3>
     <div>Record: {{ season.wins }} wins - {{ season.losses }} losses</div>
+    <div>ERA+: {{season.era_plus}}</div>
+    <div>Batting Average: {{season.bat_ave}}</div>
+    <div>OPS: {{season.ops}}</div>
+    <div>Run Differential: {{season.run_diff}}</div>
   </div>
 </template>
 
