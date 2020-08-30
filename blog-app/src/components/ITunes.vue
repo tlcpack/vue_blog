9bc6eab5c540bc022b3fd609dd22d715bed64d10
Taylor Cooke <taylorcooke@gmail.com>
2020-08-30 14:46:40 -0400

    added styling

--- blog-app/src/components/ITunes.vue
+++ blog-app/src/components/ITunes.vue
@@ -72,3 +72,80 @@
     </v-flex>
   </v-container>
 </template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+  .album-box {
+    position: relative;
+    display: inline-block;
+    box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
+    border-radius: 3px;
+    -webkit-transition: all 0.6s cubic-bezier(0.165, 0.84, 0.44, 1);
+    transition: all 0.6s cubic-bezier(0.165, 0.84, 0.44, 1);
+  }
+
+  .album-box:hover {
+    -webkit-transform: scale(1.12, 1.12);
+    transform: scale(1.12, 1.12);
+  }
+
+  .album-card {
+    width: 160px;
+  }
+
+  .album-card:hover {
+    cursor: pointer;
+  }
+
+  .album-card:hover .play-btn {
+    display: block;
+  }
+
+  .album-cover,
+  .album-cover img {
+    border-radius: 2px !important;
+  }
+
+  .album-cover:active {
+    animation: pop 0.3s linear 1;
+  }
+
+  @keyframes pop{
+    50%  { transform: scale(1.02); }
+  }
+
+  .album-info div {
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+
+  .v-card {
+    background: #1C1F37;
+    height: 100%;
+  }
+
+  .v-card__title,
+  .v-card__text {
+    background: #262B48;
+  }
+
+  .play-btn {
+    padding-top: 110px;
+    padding-right: 100px;
+    position: absolute;
+    right: 0;
+    top: 0;
+    display: none;
+    z-index: 9;
+  }
+
+  .v-input--radio-group .v-radio {
+       margin-bottom: 8px;
+  }
+</style>
