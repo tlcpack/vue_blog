f52ce9b03a217f63a8e24454ca9c9aad8ba14c5f
Taylor Cooke <taylorcooke@gmail.com>
2020-09-02 20:53:15 -0400

    fixed endpoint addresses

--- blog-app/src/components/Create.vue
+++ blog-app/src/components/Create.vue
@@ -58,7 +58,7 @@ export default {
     methods: {
         create: function () {
             this.submitted = true;
-            axios.post("nameless-spire-79883.herokuapp.com/api/posts/", this.post)
+            axios.post("http://nameless-spire-79883.herokuapp.com/api/posts/", this.post)
             .then((response) => {
                 console.log(response);
                 this.$router.push("/");
